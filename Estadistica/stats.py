fa8357df0ef021d8aef78891be7e8618444d2daa
albeiro036 <albeiro036@gmail.com>
2026-09-10 22:12:21 -0500

    dev

---
+++ Estadistica/stats.py
@@ -0,0 +1,4 @@
+"""
+Las muestras se reúnen a partir de poblaciones, que son conjuntos de todos los individuos o elementos individuales de un tipo específico.
+A veces una población representa un sistema científico
+"""
--- functions/featuresCorrelation.py
+++ functions/featuresCorrelation.py
@@ -128,11 +128,11 @@ class Features_correlations_pearson(FeaturesHandling):
         )
         matrix = numeric_features.corr(method="pearson").round(2)
         if interpret:
-            return matrix.apply(lambda x: x.apply(self.__interpret_pearson_value))
+            return matrix.apply(lambda x: x.apply(self._interpret_pearson_value))
         return matrix
         # sns.heatmap(df.corr(), annot=True, cmap='coolwarm', fmt='.2f')
 
-    def __interpret_pearson_value(self, r):
+    def _interpret_pearson_value(self, r):
         r_abs = abs(r)
         if r_abs == 1:
             return "--"
@@ -235,11 +235,11 @@ class Features_correlations_spearman(FeaturesHandling):
         )
         matrix = numeric_features.corr(method="spearman").round(2)
         if interpret:
-            return matrix.apply(lambda x: x.apply(self.__interpret_spearman_value))
+            return matrix.apply(lambda x: x.apply(self._interpret_spearman_value))
         return matrix
         # sns.heatmap(df.corr(), annot=True, cmap='coolwarm', fmt='.2f')
 
-    def __interpret_spearman_value(self, r):
+    def _interpret_spearman_value(self, r):
         r_abs = abs(r)
         if r_abs == 1:
             return "--"
@@ -311,12 +311,13 @@ class Features_correlations_cramers_v:
             raise ValueError(
                 "Both featureAnalyze and featureCorrelation must be categorical and present in the DataFrame"
             )
-        #Creacion de la tabla de contingencia
+        # Creacion de la tabla de contingencia
         contingency_table = pd.crosstab(
             categorical_cols[featureAnalyze], categorical_cols[featureCorrelation]
         )
         # Prueba de chi-cuadrado (χ²) para la tabla de contingencia
         chi2, p, dof, expected = chi2_contingency(contingency_table)
+        # prueba de asociación usando Cramér's V
         cramers_v = association(contingency_table, method="cramer")
         if with_plot:
             import seaborn as sns
@@ -326,8 +327,51 @@ class Features_correlations_cramers_v:
             plt.title(f"Cramér's V: {cramers_v:.2f}")
             plt.show()
 
-        return cramers_v
+        return super()._config_output(
+            {
+                "featureAnalyze": featureAnalyze,
+                "featureCorrelation": featureCorrelation,
+                "chi2": chi2,
+                "p_value": p,
+                "dof": dof,
+                "expected": expected,
+                "cramers_v": cramers_v,
+            }
+        )
+    def calculate_cramers_v_matrix(self, custom_features=[], interpret=False):
+        categorical_features = super()._filter_features(
+            type="categorical", custom_features=custom_features
+        ) 
+        matrix = categorical_features.corr(method="cramer").round(2)       
+        if interpret:
+            return matrix.apply(lambda: x.apply(self._interpret_cramers_v_value))
+        return matrix
+    
+    def _interpret_cramers_v_value(self, r):
+        if r == 0:
+            return 'null'
+        elif r <= 0.1:
+            return 'very weak'
+        elif r <= 0.3:
+            return 'weak'
+        elif r <= 0.5:
+            return 'moderate'
+        elif r <= 0.7:
+            return 'strong'
+        else:
+            return 'very strong'
 
+# ****************************************************************************************
+# Theils U: también conocido como Coeficiente de Incertidumbre o Entropy Coefficient
+# -------------------------------------------------
+# H(X): Entropía de la variable X (su incertidumbre total).
+# H(X|Y): Entropía de X dado que conocemos Y (incertidumbre restante).
+# H(X) - H(X|Y): Es la Información Mutua entre X e Y.
+# Interpretación directa: U(X|Y) responde a la pregunta: "Si conozco el valor de Y,
+#  ¿en qué porcentaje se reduce mi incertidumbre sobre X?"
+# ****************************************************************************************
+class Features_correlations_theils_u(FeaturesHandling):
+    pass
 
 class FeaturesCorrelations(
     FeaturesHandling, Features_correlations_pearson, Features_correlations_spearman
